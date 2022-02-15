bd8bae8ea8725c4e0eb38696905bcb10a13ee060
omgdendi <a4baytakov@mail.ru>
2022-02-15 18:31:47 +0300

    add "id" to CategoryToGetDTO

--- src/main/java/com/omgdendi/blps/dto/CategoryToGetDTO.java
+++ src/main/java/com/omgdendi/blps/dto/CategoryToGetDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryToGetDTO {
+    private Long id;
     private String name;
     private int count;
 }
--- src/main/java/com/omgdendi/blps/mappers/CategoryToGetMapper.java
+++ src/main/java/com/omgdendi/blps/mappers/CategoryToGetMapper.java
@@ -4,11 +4,13 @@ import com.omgdendi.blps.dto.CategoryDTO;
 import com.omgdendi.blps.dto.CategoryToGetDTO;
 import com.omgdendi.blps.entity.CategoryEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CategoryToGetMapper {
     CategoryToGetMapper INSTANCE = Mappers.getMapper(CategoryToGetMapper.class);
 
+    @Mapping(source = "id", target = "id")
     CategoryToGetDTO toDTO(CategoryEntity categoryEntity);
 }
